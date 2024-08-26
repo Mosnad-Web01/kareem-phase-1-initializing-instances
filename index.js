e802cf2e6ee9a7db17b4d33f032d543dbc44464e
kareemAlqubati <salamkareem792@gmail.com>
2024-08-26 12:00:34 +0300

    Compelet

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here
+    function Breakfast(food, drink) {
+    this.food = food
+    this.drink = drink
+}
+
+class Lunch extends Breakfast {
+    constructor(salad, soup, drink) {
+        super()
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+class Dinner extends Lunch {
+    constructor(salad, soup, entree, dessert) {
+        super()
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.dessert = dessert
+    }
+}
+
